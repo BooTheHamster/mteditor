7efe0b77e72fb3d1b3b8c8636b705b96fc5363ba
Oleg Sevostyanov <oleg.sev@gmail.com>
2020-11-12 19:17:50 +0400

    Загрузка редактируемых записей из файла и отправка на клиент

--- Services/EditFileService.cs
+++ Services/EditFileService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
@@ -15,6 +16,10 @@ namespace mteditor.Services
     /// </summary>
     public class EditFileService
     {
+        /// <summary>
+        /// Разделитель между частью имени файла и каталогом в файле.
+        /// </summary>
+        private const char Delimiter = '=';
         private readonly ILogger<EditFileService> _logger;
         private readonly IHubContext<EditFileHub, IEditFileHub> _editFileHub;
         private string _editFilePath;
@@ -33,15 +38,54 @@ namespace mteditor.Services
 
         public IEnumerable<EditRecord> GetRecords()
         {
-            return Enumerable.Range(1, 10)
-                .Select(i => new EditRecord($"FilenamePart_{i}", $"{_editFilePath}"))
-                .ToArray();
+            if (!File.Exists(_editFilePath))
+            {
+                return Enumerable.Empty<EditRecord>();
+            }
+
+            return GetRecordsFromFile();
+        }
+
+        private EditRecord ParseLine(string line)
+        {
+            var parts = line.Split(Delimiter);
+            return new EditRecord(parts[0], parts[1]);
+        }
+
+        private IEnumerable<EditRecord> GetRecordsFromFile()
+        {
+            try
+            {
+                var result = new List<EditRecord>();
+
+                foreach (var line in File.ReadLines(_editFilePath))
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        continue;
+                    }
+
+                    var editRecord = ParseLine(line);
+
+                    if (editRecord == null)
+                    {
+                        continue;
+                    }
+
+                    result.Add(editRecord);
+                }                
+
+                return result;
+            }
+            catch (System.Exception error)
+            {
+                _logger.LogError(error, "Error read file");
+                return Enumerable.Empty<EditRecord>();
+            }
         }
 
         private void OnEditFileSettings(EditFileSettings settings, string arg)
         {
-            _logger.LogInformation("OnEditFileSettings");
-            
             _editFilePath = settings.EditFilePath;
             _editFileHub.Clients.All.ReloadRecords();
         }
