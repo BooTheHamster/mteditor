eb0df3f5b8874cc9522bba2b9bc2acc921d07eed
Oleg Sevostyanov <oleg.sev@gmail.com>
2020-12-03 13:55:12 +0400

    Добавление оформления полей для редактирования с помощью Angular Material

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { EditFileHubClientService } from './Services/edit-file-hub-client.service';
 import { RecordsEditServiceService } from './Services/records-edit.service';
 import { EditRecordComponent } from './Components/edit-record/edit-record.component';
 import { EditRecordTitleComponent } from './Components/edit-record-title/edit-record-title.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { EditRecordTitleComponent } from './Components/edit-record-title/edit-re
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [
     RecordsEditServiceService,
