9e43c9c1dd8564f6632e98dd3d8ccb6b736afbfe
Oleg Sevostyanov <oleg.sev@gmail.com>
2020-12-04 22:35:47 +0400

    Миграция приложения на .Net 5

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http.Connections;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
